b9738389139aadccfa6189af3b8bbb25bfd3c0ce
tonilin <tonilin@gmail.com>
2014-08-22 15:11:49 +0800

    add relations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: games
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text
+#  user_id     :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Game < ActiveRecord::Base
+  belongs_to :user
+
+
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: games
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text
+#  user_id     :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'rails_helper'
 
 RSpec.describe Game, :type => :model do
