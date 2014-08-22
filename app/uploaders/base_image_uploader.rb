e95dce9f9efa28863505abdb67d205e0643676bc
tonilin <tonilin@gmail.com>
2014-08-22 17:42:40 +0800

    debug

--- app/uploaders/base_image_uploader.rb
+++ app/uploaders/base_image_uploader.rb
@@ -41,6 +41,16 @@ class BaseImageUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
+  def filename
+     "#{secure_token(10)}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token(length=16)
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
+  end
+
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
