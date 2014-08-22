b039f10342749c395d9d3265d97e957f73af0302
tonilin <tonilin@gmail.com>
2014-08-22 15:09:39 +0800

    add game model

--- db/migrate/20140822070414_create_games.rb
+++ db/migrate/20140822070414_create_games.rb
@@ -1,7 +1,9 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-
+      t.string  :title
+      t.text    :description
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822065330) do
+ActiveRecord::Schema.define(version: 20140822070414) do
+
+  create_table "games", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
